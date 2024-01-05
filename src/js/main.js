3df0d2ab0b12373fb47c9fd0c459cf25d836fd4f
N3nT <41384370+N3nT@users.noreply.github.com>
2024-01-05 14:45:51 +0100

    Mobile menu should be not scrolable, should be...

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,23 @@ const footerYear = document.querySelector('.footer-year')
 const mobileNavButton = document.querySelector('.nav__mobile-button')
 const mobileNav = document.querySelector('.nav__mobile')
 const mobileNavItems = document.querySelectorAll('.nav__mobile-item')
+const body = document.querySelector('body')
 
 const mobileNavHandler = () => {
-  console.log('gut');
-  mobileNav.classList.add('nav__mobile--active')
-  mobileNavItems.forEach(item => {
-    item.addEventListener('click', () => {
-      mobileNav.classList.remove('nav__mobile--active')
-    })
-  });
+	console.log('gut')
+  body.classList.add('fixed-position')
+	mobileNav.classList.add('nav__mobile--active')
+	mobileNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			mobileNav.classList.remove('nav__mobile--active')
+			body.classList.remove('fixed-position')
+		})
+	})
 }
 
 const setYear = (params) => {
-    const date = new Date()
-    footerYear.textContent = date.getFullYear()
+	const date = new Date()
+	footerYear.textContent = date.getFullYear()
 }
 
 document.addEventListener('DOMContentLoaded', setYear)
